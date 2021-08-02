05999aff70854bba9da978534252f8e7ff30e9dd
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-08-02 17:00:48 +0900

    詳細機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,10 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def destroy
     article = Article.find(params[:id])
     article.destroy
