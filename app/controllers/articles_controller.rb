649ea335f14ab8148f5ffdb5ec334de01047e9a2
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-08-02 16:31:16 +0900

    投稿編集機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    if article.update(article_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     article = Article.find(params[:id])
     article.destroy
